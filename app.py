dc6fdd8a9bbf40ade54e8548dc999d65b01dd04c
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2024-10-01 23:32:17 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1090,9 +1090,10 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-
+    app.run(debug=True)  # Use debug=True for local development
+    
     # Ensure the upload folder exists
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(uploads)
+#    if not os.path.exists(UPLOAD_FOLDER):
+#        os.makedirs(uploads)
 
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))  # Use environment variable for port
+#    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))  # Use environment variable for port
