3d21b5a61adfbb7143748c6ab4548a5a089e93f4
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2024-11-07 10:30:43 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -205,6 +205,7 @@ def team_ranking():
              .join(Job_Tracking, Job_Tracking.Job_ID == job_team_members.Job_ID) \
              .join(client_unique_days_query, client_unique_days_query.c.Client_Unique_ID == Job_Tracking.Client_Unique_ID) \
              .filter(Job_Tracking.Date.between(start_date, end_date)) \
+             .filter(Job_Tracking.Team_Member_ID == Team_Members.Team_Member_ID) \
              .group_by(Team_Members.Team_Member_Name).subquery()
 
             # Subquery to calculate days worked for each team member
