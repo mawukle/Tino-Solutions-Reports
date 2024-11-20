9a869eb55f5ef79eb9a704aa6db84012bc27e8c0
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2024-11-20 20:01:05 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -2111,7 +2111,7 @@ if __name__ == '__main__':
 
     # Use the port from environment variables; default to 5000 for local development
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 #    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))  # Use environment variable for port
