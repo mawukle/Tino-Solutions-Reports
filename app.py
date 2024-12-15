625d439269d5612ec8c92e936e92961c7cdc8aa7
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2024-12-15 13:06:30 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -340,27 +340,10 @@ def team_ranking():
                 client_items.date.between(start_date, end_date)
             ).group_by(Client_List.Client_Name).all()
 
-            # Convert capacities to desired decimal places
-            formatted_inverter_data = [
-                {
-                    'Client_Name': item.Client_Name,
-                    'total_inverter_capacity': round(item.total_inverter_capacity, 2) if item.total_inverter_capacity else 0
-                } for item in inverter_data
-            ]
-
-            formatted_battery_data = [
-                {
-                    'Client_Name': item.Client_Name,
-                    'total_battery_capacity': round(item.total_battery_capacity, 2) if item.total_battery_capacity else 0
-                } for item in battery_data
-            ]
-
-            formatted_solar_panel_data = [
-                {
-                    'Client_Name': item.Client_Name,
-                    'total_solar_panel_capacity': round(item.total_solar_panel_capacity, 3) if item.total_solar_panel_capacity else 0
-                } for item in solar_panel_data
-            ]
+            # Round off the capacities to 2 decimal places
+            total_solar_panel_capacity = round(total_solar_panel_capacity, 2)
+            total_inverter_capacity = round(total_inverter_capacity, 2)
+            total_battery_capacity = round(total_battery_capacity, 2)
 
             # CTE to get the unique clients visited by each team member
             team_member_clients_query = db.session.query(
