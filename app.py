f904fd3c9ef4175c45c69e5f36667b0b7492f114
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2025-01-16 16:52:15 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ def graphical_reports():
             }
 
             inverter_quantity_chart_data = {
-                'labels': list(aggregated_inverter_quantity.keys()),
+                'labels': [{'Item_Description': desc} for desc in aggregated_inverter_quantity.keys()],
                 'data': [float(value) for value in aggregated_inverter_quantity.values()]  # Convert Decimal to float
             }
 
