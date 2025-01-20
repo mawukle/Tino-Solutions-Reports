7d4e8913f0fe3c02126b9babdc6db1cbe81e33b4
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2025-01-20 20:05:56 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -657,14 +657,19 @@ def team_ranking():
                     "total_quantity": row.total_quantity
                 }
                 for row in db.session.query(
-                    func.max(Items_List.Item_Description).label('Item_Description'),
-                    func.sum(sales_by_item.qty_sold).label('total_quantity')
+                    Items_List.Item_Description.label('Item_Description'),
+                    func.sum(client_items.quantity).label('total_quantity')
                 ).join(
-                    sales_by_item, Items_List.Item_Description == sales_by_item.item_description
+                    client_items, or_(
+                        client_items.item_description == Items_List.Item_Description,
+                        client_items.item_description == Items_List.Alias_Description
+                    )
                 ).filter(
                     Items_List.Component == 'Batteries',
-                    sales_by_item.date.between(start_date, end_date)
-                ).group_by(Items_List.Item_Description).order_by(desc('total_quantity')).all()
+                    client_items.date.between(start_date, end_date)
+                ).group_by(Items_List.Item_Description)
+                .order_by(desc('total_quantity'))
+                .all()
             ]
 
             # Query for Solar Panel Quantities
