f1fd6d6199d1c82f45b517f353f6b847625270d8
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2025-03-03 12:59:16 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -3973,7 +3973,7 @@ def send_project_email_notification(project):
         msg = Message(
             subject,
             recipients=[sales_person_email],
-            cc=["padiemmanuelkwesi@gmail.com", "emmanuel@tinosolutions"],
+            cc=["marketing@tinosolutions.com", "ebenezer@tinosolutions.com", "augustine@tinosolutions.com"],
             html=body
         )
 
@@ -4020,7 +4020,7 @@ def send_completed_project_email_notification(project):
         msg = Message(
             subject,
             recipients=[sales_person_email],
-            cc=["padiemmanuelkwesi@gmail.com", "padiemmanuelkwesi@yahoo.com"],
+            cc=["marketing@tinosolutions.com", "ebenezer@tinosolutions.com", "augustine@tinosolutions.com", "hippolite@tinosolutions.com", "support@tinosolutions.com", "solal@tinosolutions.com", "accounts@tinosolutions.com", "gorden@tinosolutions.com", "postino@tinosolutions.com"],
             html=body
         )
 
