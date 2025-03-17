983a56edb24899a30e8110c2b458b6a9b5762f7a
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2025-03-17 13:45:22 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -4181,10 +4181,10 @@ def get_bdu():
             invoice_amount = Decimal(project.invoice_amount or 0.00)
             amount_paid = Decimal(project.amount_paid or 0.00)
             outstanding_balance = invoice_amount - amount_paid  # Dynamically calculate Outstanding Balance
-            if project.commissioning_date:
+            if isinstance(project.commissioning_date, str):
                 commissioning_date = datetime.strptime(project.commissioning_date, '%Y-%m-%d').date()
             else:
-                commissioning_date = None
+                commissioning_date = project.commissioning_date  # Already a datetime.date
             expected_payment_date = project.expected_final_payment_date.strftime('%Y-%m-%d') if project.expected_final_payment_date else ''
 
             project_data = {
