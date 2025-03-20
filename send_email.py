d1a498704060692fa7b72a1d9f1dd4985e3bd06b
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2025-03-20 14:53:41 +0000

    Updated app.py

--- send_email.py
+++ send_email.py
@@ -107,7 +107,8 @@ def send_payment_reminders():
                     should_send_email = True
 
             elif project.commissioning_date:
-                due_date = project.commissioning_date + datetime.timedelta(days=14)
+                commissioning_date = datetime.datetime.strptime(project.commissioning_date, "%Y-%m-%d").date()
+                due_date = commissioning_date + datetime.timedelta(days=14)
                 if due_date == today or (today > due_date and (today - due_date).days % 7 == 0):
                     should_send_email = True
 
