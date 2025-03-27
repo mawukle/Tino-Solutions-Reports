9c1c4dc24b6cf00d360defeb0bebfed894989e2b
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2025-03-27 21:50:11 +0000

    Updated app.py

--- drive_uploader.py
+++ drive_uploader.py
@@ -10,14 +10,14 @@ GOOGLE_CREDENTIALS_FILE = "tinosolutions-invoices-d422558b4d05.json"  # Change t
 # Set the Google Drive folder ID where images will be stored
 GOOGLE_DRIVE_FOLDER_ID = "15ANbwh6M8c7eAp_o8vWToOHs-ObjdLP9"  # Change this to your folder ID
 
-def upload_to_drive(file_path, file_name):
+def upload_to_drive(file_path, file_name, folder_id):
     """Uploads a file to Google Drive and returns the file URL."""
     credentials = Credentials.from_service_account_file(GOOGLE_CREDENTIALS_FILE, scopes=["https://www.googleapis.com/auth/drive.file"])
     service = build("drive", "v3", credentials=credentials)
 
     file_metadata = {
         "name": file_name,
-        "parents": [GOOGLE_DRIVE_FOLDER_ID]
+        "parents": [folder_id]  # Use provided folder ID instead of hardcoded `GOOGLE_DRIVE_FOLDER_ID`
     }
 
     media = MediaFileUpload(file_path, mimetype="image/jpeg")
