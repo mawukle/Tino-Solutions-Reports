3e8ce1e40f8b1244507aef651a6439e22bb51388
Emmanuel Kwesi Padi <tinosolutionslimited@Emmanuels-MacBook-Pro.local>
2025-04-14 20:38:08 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -4080,7 +4080,7 @@ def send_project_email_notification(project):
             recipients=[sales_person_email],
             #cc=["emmanuel@tinosolutions.com"],
             cc=["augustine@tinosolutions.com"],
-            bcc=["marketing@tinosolutions.com", "ebenezer@tinosolutions.com", "emmanuel@tinosolutions.com"],  # Add BCC recipients
+            bcc=["ebenezer@tinosolutions.com", "emmanuel@tinosolutions.com"],  # Add BCC recipients
             html=body
         )
 
